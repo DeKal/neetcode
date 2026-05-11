c0fd16cf9860d41db285eadd76322633c01827b4
Huu-Phat Ho <hohuuphat22@gmail.com>
2026-05-11 10:52:41 +0700

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = one = 0
+        for two in range(len(nums)):
+            # write 2
+            tmp = nums[two]
+            nums[two] = 2
+
+            # write 1 will override the next 2
+            if tmp < 2:
+                nums[one] = 1
+                one += 1
+            # write 0 will override the next 1
+            if tmp < 1:
+                nums[zero] = 0
+                zero += 1
